4fd0d9d9e859693d53644f72534a4200ddea086d
Rachel White <loveless@gmail.com>
2016-05-23 14:28:27 -0400

    added linting and proper generator input plugin structure. still need tests

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# input-plugin-year
+Year Input Plugin [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+---
+
 A simple select input for years based on a range
+
+## License
+
+Apache-2.0
+
+[npm-image]: https://badge.fury.io/js/input-plugin-year.svg
+[npm-url]: https://npmjs.org/package/input-plugin-year
+[travis-image]: https://travis-ci.org/punchcard-cms/input-plugin-year.svg
+[travis-url]: https://travis-ci.org/punchcard-cms/input-plugin-year
+[daviddm-image]: https://david-dm.org/punchcard-cms/input-plugin-year.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/punchcard-cms/input-plugin-year
+[coveralls-image]: https://coveralls.io/repos/punchcard-cms/input-plugin-year/badge.svg
+[coveralls-url]: https://coveralls.io/r/punchcard-cms/input-plugin-year
---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * year Input Plugin
+ *
+ *
+ * A simple select input for years based on a range
+ */
+const validation = require('./lib/validation.js');
+
+/**
+ * Single year Input Plugin
+ * @module yearInputPlugin
+ */
+module.exports = {
+  name: 'year',
+  description: 'A simple select input for years based on a range',
+  validation: {
+    yearValidation: validation,
+  },
+  inputs: {
+    year: {
+      validation: {
+        function: 'yearValidation',
+        on: 'change',
+      },
+      label: 'Please choose a year',
+      placeholder: 'year',
+      options: {
+        minYear: '1900',
+        maxYear: '2016',
+      },
+      type: 'select',
+      settings: {
+        empty: true,
+      },
+    },
+  },
+  html: '<label for="{{year.id}}">{{year.label}}</label><select id="{{year.id}}" name="{{year.name}}"><option value="{{year.value}}"  class="year-range"></option></select></label>',
+};
